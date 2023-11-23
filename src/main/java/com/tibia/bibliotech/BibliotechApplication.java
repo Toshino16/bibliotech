c8f11ec0ebb160465be4ba50215c49375b933096
AlvesLuis0 <alves.luis.3002@gmail.com>
2023-11-23 18:51:15 -0300

    commit do Luis

--- src/main/java/com/tibia/bibliotech/BibliotechApplication.java
+++ src/main/java/com/tibia/bibliotech/BibliotechApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BibliotechApplication {
 
   public static void main(String[] args) {
+    // Luis esteve por aqui
     SpringApplication.run(BibliotechApplication.class, args);
   }
 

b92b0da93e6c27317a86b198ae9e52076d6129a5
AlvesLuis0 <alves.luis.3002@gmail.com>
2023-11-24 14:05:20 -0300

    Usuario serviço criado

---
+++ src/main/java/com/tibia/bibliotech/servicos/UsuarioServico.java
@@ -0,0 +1,38 @@
+package com.tibia.bibliotech.servicos;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tibia.bibliotech.modelos.Usuario;
+import com.tibia.bibliotech.repositorios.UsuarioRepositorio;
+
+@Service
+public class UsuarioServico {
+
+  @Autowired
+  private UsuarioRepositorio usuarioRepositorio;
+
+  public List<Usuario> lista() {
+    return usuarioRepositorio.findAll();
+  }
+
+  public Usuario buscarPeloId(Long id) {
+    return usuarioRepositorio.findById(id).get();
+  }
+
+  public Usuario criar(Usuario usuario) {
+    return usuarioRepositorio.save(usuario);
+  }
+
+  public Usuario atualizarPeloId(Usuario usuario, Long id) {
+    usuario.setId(id);
+    return usuarioRepositorio.save(usuario);
+  }
+
+  public void deletarPeloId(Long id) {
+    usuarioRepositorio.deleteById(id);
+  }
+  
+}

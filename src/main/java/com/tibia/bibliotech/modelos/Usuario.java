9254893e5e500a7f551e9d9636f16ebc4da71968
AlvesLuis0 <alves.luis.3002@gmail.com>
2023-11-28 10:46:37 -0300

    Usuário com novos métodos

--- src/main/java/com/tibia/bibliotech/modelos/Usuario.java
+++ src/main/java/com/tibia/bibliotech/modelos/Usuario.java
@@ -1,8 +1,13 @@
 package com.tibia.bibliotech.modelos;
 
+import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -11,7 +16,7 @@ import jakarta.persistence.Id;
 import lombok.Data;
 
 @Entity @Data
-public class Usuario {
+public class Usuario implements UserDetails {
 
   @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
@@ -20,5 +25,40 @@ public class Usuario {
   private String senha;
   @DateTimeFormat(pattern = "dd/MM/yyyy")
   private Date criadoEm;
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    if(nome == "admin") return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+    return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+  }
+  @Override
+  public String getPassword() {
+    return senha;
+  }
+
+  @Override
+  public String getUsername() {
+    return nome;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
   
 }
